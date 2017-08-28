4eac778cb92413b6c39202a2e5db35f71f21fee9
Kelly Stoops <kstoops815@gmail.com>
2017-08-28 18:48:10 -0500

    reading from dogs

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 console.log("hello");
+
+function executeThisCodeAfterFileLoads(){
+	var data = JSON.parse(this.responseText);
+	console.log("dogs", data.dogs);
+}
+
+function executeThisCodeIfFileErrors(){
+	console.log("Shit broke");
+}
+
+var myRequest = new XMLHttpRequest();
+myRequest.addEventListener("load", executeThisCodeAfterFileLoads);
+myRequest.addEventListener("error", executeThisCodeIfFileErrors);
+myRequest.open("GET", "dogs.json");
+myRequest.send();
+
