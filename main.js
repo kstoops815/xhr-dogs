e7f57f5e463c1a98c06bcca200e830aebd4f5a81
Kelly Stoops <kstoops815@gmail.com>
2017-08-28 19:10:13 -0500

    added breeds.json call

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
 console.log("hello");
 
 function executeThisCodeAfterFileLoads(){
-	var data = JSON.parse(this.responseText);
-	console.log("dogs", data.dogs);
+	var data = JSON.parse(this.responseText).dogs;
+	console.log("dogs", data);
 }
 
 function executeThisCodeIfFileErrors(){
 	console.log("Shit broke");
 }
 
-var myRequest = new XMLHttpRequest();
-myRequest.addEventListener("load", executeThisCodeAfterFileLoads);
-myRequest.addEventListener("error", executeThisCodeIfFileErrors);
-myRequest.open("GET", "dogs.json");
-myRequest.send();
+var myDogs = new XMLHttpRequest();
+myDogs.addEventListener("load", executeThisCodeAfterFileLoads);
+myDogs.addEventListener("error", executeThisCodeIfFileErrors);
+myDogs.open("GET", "dogs.json");
+myDogs.send();
 
+function executeThisCodeAfterFileLoads2(){
+	var data = JSON.parse(this.responseText).breeds;
+	console.log("breeds", data);
+}
+
+var myBreeds = new XMLHttpRequest();
+myBreeds.addEventListener("load", executeThisCodeAfterFileLoads2);
+myBreeds.addEventListener("error", executeThisCodeIfFileErrors);
+myBreeds.open("GET", "breeds.json");
+myBreeds.send();
